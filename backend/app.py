6af04d252e1eeeb8b3e21a74ecdd1fd4557aceba
Raghupatruni Varshitha <varshitharaghupatruni@gmail.com>
2026-07-02 20:32:02 +0530

    Prepare project for deployment

--- backend/app.py
+++ backend/app.py
@@ -27,6 +27,8 @@ def analyze():
     result = trisense_score(headline, article)
     return jsonify(result), 200
 
+import os
+
 if __name__ == "__main__":
-    # change host='0.0.0.0' if you want to expose to network
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
